23ba933773b7f710e7d578322888231ece733be8
Caleb0206 <calebhuang0206@gmail.com>
2026-09-12 16:35:15 -0700

    Initial home page, Next.js project files

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="background">
+      <main>
+        <h1>Arco Scales</h1>
+        <p>A progress tracker of music scales for the viola.</p>
+
+      </main>
+    </div>
+  );
+}
