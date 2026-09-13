7fda7d4fcafefa14307e8c565a76288d8cf3f5a0
Caleb0206 <calebhuang0206@gmail.com>
2026-09-12 17:07:17 -0700

    navbar temporary

---
+++ components/Navigation.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Navigation() {
+    return (
+        <header className="site-header">
+            <nav className="site-nav" aria-label="Main navigation">
+                <Link className="nav-item" href="/">Arco Scales </Link>
+                <Link className="nav-item" href="/practice">Practice</Link>
+                <Link className="nav-item" href="/scales">Scale Library</Link>
+                <Link className="nav-item" href="/progress">Progress</Link>
+            </nav>
+        </header>
+    )
+}
